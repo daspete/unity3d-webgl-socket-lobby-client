5ab87229f9f5a60f47c8221117edd24d33d85ce0
DasPeTe <daspetemail@gmail.com>
2016-10-23 20:01:08 +0200

    game client update

--- src/Game/GameClient.js
+++ src/Game/GameClient.js
@@ -13,28 +13,10 @@ class GameClient {
         this.socket = data.socket;
         this.roomID = data.roomID;
 
-        this.socket.on('receive-game-data', this.OnReceiveGameData.bind(this));
-
         this.StartClient();
     }
 
     StartClient(){}
-
-    OnReceiveGameData(data){
-        this.SendToGame('SocketReceiveData', data);
-    }
-
-    SendToAllPlayers(data){
-        this.socket.emit('send-to-all-players', data);
-    }
-
-    SendToOnePlayer(data){
-        this.socket.emit('send-to-one-player', data);
-    }
-
-    SendToServer(data){
-        this.socket.emit('send-to-server', data);
-    }
 
     SendToGame(functionName, parameter){
         SendMessage(this.unityGameReceiverObject, functionName, parameter);
--- src/Game/MyGameClient.js
+++ src/Game/MyGameClient.js
@@ -4,10 +4,24 @@ class MyGameClient extends GameClient {
 
     StartClient(){
         this.socket.on('set-goal-position', this.OnSetGoalPosition.bind(this));
+        this.socket.on('player-won-round', this.OnPlayerWonRound.bind(this));
+        this.socket.on('player-lost-round', this.OnPlayerLostRound.bind(this));
+    }
+
+    SendPlayerClick(data){
+        this.socket.emit('player-got-position', data);
     }
 
     OnSetGoalPosition(data){
         this.SendToGame('SocketSetGoalPosition', JSON.stringify(data));
+    }
+
+    OnPlayerWonRound(){
+        this.SendToGame('SocketPlayerWonRound');
+    }
+
+    OnPlayerLostRound(){
+        this.SendToGame('SocketPlayerLostRound');
     }
 
 }
